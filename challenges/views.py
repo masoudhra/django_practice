824d98f54aeb71dd02a1b796fe045acdaed844e1
Masoud Ramezani <masoudhra@gmail.com>
2023-10-17 17:11:37 +1300

    use reverse and html in django

--- challenges/views.py
+++ challenges/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
 
@@ -14,17 +15,32 @@ days = {
 }
 
 
+def days_list(request):
+
+    days_list = list(days.keys())
+    list_items = ""
+    for day in days_list:
+        url_path = reverse('days-of-week', args=[day])
+        list_items += f'<li> <a href="{url_path}"> {day} </a> </li>\n'
+
+    content = f'<ul>\n {list_items}\n</ul>'
+
+    return HttpResponse(content)
+
+
 def dynamic_days_by_number(request, day):
     days_names = list(days.keys())
     if day > len(days_names):
         return HttpResponseNotFound('day does not exists')
     redirect_day = days_names[day - 1]
-    return HttpResponseRedirect(f'/days/{redirect_day}')
+    redirect_url = reverse('days-of-week', args=[redirect_day])
+    return HttpResponseRedirect(redirect_url)
     # return HttpResponse(day)
 
 
 def dynamic_days(reqeust, day):
     day_data = days.get(day)
     if day_data is not None:
-        return HttpResponse(f'day is : {day} and data is : {day_data}')
+        response_data = f'<h1 style="color:red"> day is : {day} and data is : {day_data} </h1>'
+        return HttpResponse(response_data)
     return HttpResponseNotFound('day does not exists')
